ea8d144f6f9f58975c50270751643347ccc724aa
Dustin Iser <diser@confluence.com>
2015-06-16 00:09:23 -0400

    Add some steps.

--- NightwatchDemo/step-definitions/navigation.js
+++ NightwatchDemo/step-definitions/navigation.js
@@ -5,4 +5,12 @@ module.exports = function () {
     this.Given(/^I visit the home page$/, function () {
         this.url(config.Url);
     })
+
+    this.When(/^I click on the home button$/, function () {
+        var xpath = "//*[contains(concat(' ', normalize-space(@class), ' '), ' navbar-brand ')]";
+
+        this.useXpath()
+            .waitForElementVisible(xpath, config.TimeOut)
+            .click(xpath);
+    })
 }
