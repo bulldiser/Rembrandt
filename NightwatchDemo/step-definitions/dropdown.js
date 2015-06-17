b34e8807d81ceed54af958e408663dc6317c6531
pnityau <pnityau@gmailo.com>
2015-06-17 15:53:07 -0400

    Add functionality for manipulating dropdowns.

---
+++ NightwatchDemo/step-definitions/dropdown.js
@@ -0,0 +1,22 @@
+﻿var assert = require('assert'),
+    driver = require('../globals/driverextensions'),
+    config = require('../config/config.json');
+
+module.exports = function () {
+
+    this.When(/^I select "(.*)" from the "(.*)"$/, function (value, field) {
+
+        var xpath = "//*[text()='" + field + "']";
+
+
+        this.useXpath()
+            .waitForElementVisible(xpath, config.TimeOut)
+            .element('xpath', xpath, function (result) {
+                this.elementIdAttribute(result.value.ELEMENT, 'for', function (result) {
+                    xpath = '//*[@id="' + result.value + '"]';
+                    this.waitForElementVisible(xpath, config.TimeOut)
+                        .setValue(xpath, value);
+                });
+            });
+    })
+}
