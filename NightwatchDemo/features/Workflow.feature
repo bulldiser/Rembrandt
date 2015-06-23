be3c71ffb23ff91988f6c8b2dc572ca477e9b398
pnityau <pnityau@gmailo.com>
2015-06-23 11:02:50 -0400

    Adding a test

---
+++ NightwatchDemo/features/Workflow.feature
@@ -0,0 +1,7 @@
+﻿Feature: Workflow
+Validates workflow page functionality
+	Scenario: Validate navigation to create workflow page
+		Given I visit the home page
+		When I click "CREATE" from the "WORKFLOW" in top menu
+		Then I should see the page titled "Create Workflow | Data Management Platform"
+
--- NightwatchDemo/step-definitions/formsteps.js
+++ NightwatchDemo/step-definitions/formsteps.js
@@ -1,13 +1,28 @@
-﻿var config = require('../config/config.json'),
+﻿var assert = require('assert'),
+    config = require('../config/config.json'),
     driver = require('../globals/driverextensions');
 
 module.exports = function () {
 
     this.When(/^I click "(.*)"$/, function (text) {
+
         var xpath = "//*[text()='" + text + "']"
 
         this.useXpath()
             .waitForElementVisible(xpath, config.TimeOut)
             .click(xpath);
-    })
-}
+    });
+
+    this.When(/^I click "([^"]*)" from the "([^"]*)" in top menu$/, function (subMenu, topMenu) {
+        var topMenuXpath = "//*[text()='" + topMenu + "']";
+        var subMenuXpath = "//*[text()='" + topMenu + "']/ancestor::li/descendant::*[text()='" + subMenu + "']";
+
+        this.useXpath()
+            .waitForElementVisible(topMenuXpath, config.TimeOut)
+            .click(topMenuXpath)
+            .waitForElementVisible(subMenuXpath, config.TimeOut)
+            .click(subMenuXpath);
+        
+    });
+
+     }
